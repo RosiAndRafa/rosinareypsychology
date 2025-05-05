a2b06818a184244eaf13d99e5721f45233e943dc
RosiAndRafa <rafaaa006@gmail.com>
2025-05-05 21:23:42 +0200

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -65,7 +65,7 @@ const translations = {
     "index_key21": "✓ Terapia para Padres con Hijos Adictos",
     "index_key22": "✓ Talleres de Estimulación Cognitiva para adultos",
     "index_key23": "✓Orientación Vocacional y Profesional",
-    "index_key24": "No se trata de lo que nos sucede, sino de cómo respondemos a lo que nos sucede.– Epicteto",
+    "index_key24": "No se trata de lo que nos sucede, sino de cómo respondemos a lo que nos sucede.– Epicteto"
   },
   en: {
     "404_key1": "Home",
@@ -132,7 +132,7 @@ const translations = {
     "index_key21": "✓ Therapy for Parents of Addicted Children",
     "index_key22": "✓ Cognitive Stimulation Workshops for Adults",
     "index_key23": "✓ Vocational and Career Guidance",
-    "index_key24": "It's not what happens to us, but how we respond to what happens to us. – Epictetus",
+    "index_key24": "It's not what happens to us, but how we respond to what happens to us. – Epictetus"
 }
   }
 };
